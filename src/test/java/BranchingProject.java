75c703c9763ed457d23bebe2535f9ff6726cd885
Merve San <sanmerve@hotmail.com.tr>
2021-05-25 19:23:44 -0400

    shared on github

--- src/test/java/BranchingProject.java
+++ src/test/java/BranchingProject.java
@@ -1,5 +1,9 @@
 public class BranchingProject {
     public static void main(String[] args) {
-
+        //created new project
+        //enables version control
+        //added .gitognore with .idea, target, and iml
+        //made an intial commmit
+        //shared project on github
     }
 }
